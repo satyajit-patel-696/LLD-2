17409823ace86e447446a5e1aeb1b77b612ec10d
satyajit <satyajeetpatel96@gmail.com>
2026-04-14 10:37:19 +0530

    added singelton pattern

---
+++ DesignPatterns/singeltonpattern.py
@@ -0,0 +1,16 @@
+import threading
+class Singleton:
+    _instance=None
+    _lock=threading.Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            with cls._lock:
+                if cls._instance is None:
+                    cls._instance=super(Singleton,cls).__new__(cls)
+        return cls._instance
+if __name__=="__main__":
+    s1=Singleton()
+    s2=Singleton()
+    print(s1 is s2)
+
