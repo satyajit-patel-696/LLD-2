40a00435f115fd95bc956fce1684a075d333625d
satyajit <satyajeetpatel96@gmail.com>
2026-04-14 19:48:31 +0530

    modified adopter

--- DesignPatterns/adopter.py
+++ DesignPatterns/adopter.py
@@ -3,30 +3,30 @@ from abc import ABC, abstractmethod
 
 class Printer(ABC):
     @abstractmethod
-    def printt(self):
+    def printt(self,txt):
         pass
 
 
 class Oldadopter:
-    def write(self):
-        print("printiting from old adopter")
+    def write(self,txt):
+        print(f"printiting from old adopter {txt}")
 
 
 class NewPrinter(Printer):
-    def printt(self):
-        print("printing from new adopter")
+    def printt(self,txt):
+        print(f"printing from new adopter{txt}")
 
 
 class PrinterAdopter(Printer):
     def __init__(self, p: Oldadopter):
         self.p = p
 
-    def printt(self):
-        self.p.write()   # ✅ FIXED (removed return)
+    def printt(self,txt):
+        self.p.write(txt)   # ✅ FIXED (removed return)
 
 
 # Usage
 old = Oldadopter()
 p = PrinterAdopter(old)
 
-p.printt()
+p.printt("bhula")
